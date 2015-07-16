3b98061b4e1a1886ff965525523f168a247d3470
tianzh <435649705@qq.com>
2015-07-17 01:09:06 +0900

    Create security.js

---
+++ src/static/forward/security.js
@@ -0,0 +1,13 @@
+
+function antiXSS(str)
+{
+	//anti XSS for small title
+	return str.replace(/[^0-9a-zA-Z^-~_]/g, "-");
+}
+
+function isEmailAddress(str)
+{
+	//check email format for comment
+	var re = /^[a-zA-Z0-9-_]+@[a-zA-Z0-9-_]+(.[a-zA-Z0-9-_]+)+$/
+	return re.test(str)
+}
